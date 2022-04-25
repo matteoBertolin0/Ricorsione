4a07ee9a6ad7df43e10f97266bd2c5e77323e6a6
matbe <matbe@DESKTOP-N9EMS07.homenet.telecomitalia.it>
2022-04-25 15:17:41 +0200

    Quadrato magico

---
+++ src/main/java/it/polito/tdp/ricorsione/model/testQuadratoMagico.java
@@ -0,0 +1,12 @@
+package it.polito.tdp.ricorsione.model;
+
+public class testQuadratoMagico {
+
+	public static void main(String[] args) {
+		QuadratoMagico q = new QuadratoMagico();
+		System.out.println("Lato 3");
+		q.risolviQuadrato(4);
+
+	}
+
+}
